3a9a82f112537f67f562e45c8d84af46b4496921
bongomin <candanobongomindaniel@gmail.com>
2020-02-18 18:49:37 +0300

    chore(Project Setup): setup the project and database connections

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+require('dotenv').config();
+
+const app = express();
+
+// creating database connections
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.DATABASE,
+   {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+   },
+   (err) => {
+      if (err) {
+         console.log('error in databse connections')
+      } else {
+         console.log('database connections have been successful...')
+      }
+   })
+
+// body parser middleware
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+
+const PORT = process.env.PORT || 500;
+app.listen(PORT, () => {
+   console.log(`application is running on ..${PORT}`);
+})
